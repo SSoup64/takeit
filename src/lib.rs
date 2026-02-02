d8a4815e49439fc14e88dbf4e3c843fd83fabcbd
soupMacbook <76470587+Superetro64@users.noreply.github.com>
2026-02-02 08:53:51 +0200

    feature: Implement HandOff type

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::sync::{ Arc, Mutex };
+
+#[derive(Debug, Clone)]
+pub struct HandOff<T>(Arc<Mutex<Option<T>>>);
+
+impl<T> HandOff<T> {
+    fn new(val: T) -> Self {
+        HandOff(Arc::new(Mutex::new(Some(val))))
+    }
+
+    fn take(self) -> Option<T> {
+        if let Ok(mut locked_value) = self.0.lock() {
+            locked_value.take()
+        } else {
+            None
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +22,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_single_thread() {
+        let handoff = HandOff::new(19);
+        let handoff_clone = handoff.clone();
+        
+        assert_eq!(handoff.take(), Some(19));
+        assert_eq!(handoff_clone.take(), None);
     }
 }
